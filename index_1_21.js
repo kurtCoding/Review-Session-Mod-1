46c13eaf1381fb1dcf8f8d58fada61ab32d83beb
kurtCoding <kjulien7790@gmail.com>
2024-01-22 15:38:26 -0500

    1st attempt question 4

--- index_1_21.js
+++ index_1_21.js
@@ -59,14 +59,55 @@ console.log(longerWord(strArr));
 
 // Given an array of Objects and a target id return the first object that matches the id or undefined if no such object is found
 // Expected Structure of a SINGLE object - 
-/*
-{
+
+arrOfObj = [{
  id: "Y42C_a",
  name: "Robert",
  occupation: "Chef",
  eyeColor: "green"
+}, {id: "L92C_c",
+ name: "Doer",
+ occupation: "Chef",
+ eyeColor: "Yellow"
+}, {id: "K43C_a",
+ name: "Bert",
+ occupation: "Chef",
+ eyeColor: "green"
+}, {id: "X32K_a",
+ name: "Rob",
+ occupation: "Chef",
+ eyeColor: "green"
+}];
+arrOfObj2 = [{
+    id: "Y33a",
+    name: "Obert",
+    occupation: "Chef",
+    eyeColor: "Black"
+   }, {id: "L9_c",
+    name: "Dor",
+    occupation: "Chef",
+    eyeColor: "Yellow"
+   }, {id: "K4_a",
+    name: "Bet",
+    occupation: "Chef",
+    eyeColor: "Tan"
+   }, {id: "XK_a",
+    name: "Robby",
+    occupation: "Chef",
+    eyeColor: "Orange"
+   }]
+   
+function findMatchingObj(arr, target) {
+    for (let obj of arr) {
+        if (obj.id === target) {
+            return obj;
+        }
+    }
+    return undefined;
 }
-*/
+
+console.log(findMatchingObj(arrOfObj, "L92C_c"));
+console.log(findMatchingObj(arrOfObj2, "XK_a"));
 
 
 // Given an array of objects and a target id return a new object with only the name. The structure for a single object in the array would be as the one above. The returned object would look like this - 
