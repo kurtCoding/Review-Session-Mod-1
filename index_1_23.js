7641b6e8567f3c4e8c35bc5e75354b11599dbc99
kurtCoding <kjulien7790@gmail.com>
2024-01-24 07:37:07 -0500

    2nd attempt at question 3

--- index_1_23.js
+++ index_1_23.js
@@ -37,6 +37,14 @@ const john = {
   // 3.----------------------------------
   // Write a function that takes an array of objects and a targetId and returns a formatted string. Each object in the array will have the same structure as the objects from the previous question plus an id. Use chatGPT to create an array of 10 such objects.
   
+function formattedString(arr, targetId) {
+    for (let obj of arr) {
+    if (targetId) {
+        return `Hello ${obj.firstName} ${obj.lastName}, id '${targetId}', have a great day at your job as a ${obj.occupation}`
+    }
+  }
+}
+
   /* Example Object
   {
     id: "Tfjso_4M"
@@ -44,3 +52,66 @@ const john = {
     lastName: "Klaus",
     occupation: "Carpenter"
   }*/
+  const arr1 = [ 
+    {
+    id: "Tfjso_4M",
+    firstName: "John",
+    lastName: "Klaus",
+    occupation: "Carpenter"
+  },
+  {
+    id: "A1b2C3d4",
+    firstName: "Alice",
+    lastName: "Smith",
+    occupation: "Software Engineer"
+  },
+  {
+    id: "XyZ_123",
+    firstName: "Bob",
+    lastName: "Johnson",
+    occupation: "Graphic Designer"
+  },
+  {
+    id: "987abc",
+    firstName: "Emma",
+    lastName: "Williams",
+    occupation: "Doctor"
+  },
+  {
+    id: "qwerty123",
+    firstName: "Ryan",
+    lastName: "Miller",
+    occupation: "Teacher"
+  },
+  {
+    id: "z1y2x3",
+    firstName: "Lily",
+    lastName: "Anderson",
+    occupation: "Architect"
+  },
+  {
+    id: "123xyz",
+    firstName: "James",
+    lastName: "Brown",
+    occupation: "Chef"
+  },
+  {
+    id: "AbCdEfG",
+    firstName: "Sophie",
+    lastName: "Roberts",
+    occupation: "Marketing Specialist"
+  },
+  {
+    id: "xyz123",
+    firstName: "Daniel",
+    lastName: "Clark",
+    occupation: "Photographer"
+  },
+  {
+    id: "789abc",
+    firstName: "Grace",
+    lastName: "Davis",
+    occupation: "Financial Analyst"
+  } ]
+  
+  console.log(formattedString(arr1, "XyZ_123"));
