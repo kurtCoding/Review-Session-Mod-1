d4bdcd3df08a2591a44dd8c912b17afb71a6edc4
kurtCoding <kjulien7790@gmail.com>
2024-01-23 20:22:51 -0500

    update question 2

--- index_1_23.js
+++ index_1_23.js
@@ -30,9 +30,9 @@ const john = {
     occupation: "Teacher"
   }
   
-  console.log(formatGreeting(john)) //=== "Hello John Klaus, have a great day at your job as a Carpenter")
+  console.log(formatGreeting(john) === "Hello John Klaus, have a great day at your job as a Carpenter")
   
-  console.log(formatGreeting(rebecca)) //=== "Hello Rebecca Cohen, have a great day at your job as a Teacher")
+  console.log(formatGreeting(rebecca) === "Hello Rebecca Cohen, have a great day at your job as a Teacher")
   
   // 3.----------------------------------
   // Write a function that takes an array of objects and a targetId and returns a formatted string. Each object in the array will have the same structure as the objects from the previous question plus an id. Use chatGPT to create an array of 10 such objects.
